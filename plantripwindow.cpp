84f0fb3d38d79e340ce0547b163c2e611e4b749e
Peijun Zhao <pzhao1@saddleback.edu>
2023-02-22 11:49:54 -0800

    Adding the functionality of calculating the next college base on the distant recursively and display the final result.

--- plantripwindow.cpp
+++ plantripwindow.cpp
@@ -40,9 +40,109 @@ void planTripWindow::on_planCustomTrip_clicked()
 
 }
 
+/*
+ * checkCampusVectorNames(QVector<QString> campuses)
+ * This function will check to if all the campuses from the vector are present in the database.
+ * IN: QVector<QString>, campuses
+ * OUT: bool
+ */
+bool planTripWindow::checkCampusVectorNames(QVector<QString> campuses)
+{
+    bool found = true;
+    QString *p = campuses.begin();
+    while (p != campuses.end() && found == true)
+    {
+        if(!m_database.checkCampusName(*p))
+        {
+            found = false;
+
+        }
+        else
+        {
+            p++;
+        }
+    }
+    return found;
+}
+
 // This button when pressed will send the user to a page that will allow them to vist any colleges starting at Saddleback
 void planTripWindow::on_startTrip_clicked()
 {
+    QVector<QString> initial11{"Saddleback College",
+                               "University of California, Irvine (UCI)",
+                               "Arizona State University",
+                               "Massachusetts Institute of Technology (MIT)",
+                               "Northwestern",
+                               "Ohio State University",
+                               "University of  Michigan",
+                               "University of California, Los Angeles (UCLA)",
+                               "University of Oregon",
+                               "University of Wisconsin",
+                               "University of the Pacific",
+                               };
 
+    bool allFound = checkCampusVectorNames(initial11);
+
+    if(!allFound){
+        QMessageBox::warning(this, "ERROR", "The 11 initial college are incomplete, please check with the administrator.", QMessageBox::Ok, QMessageBox::NoButton);
+    }else{
+        selectedCampuses.append(initial11);
+
+        QString start = *selectedCampuses.begin();
+        recursiveCollegeSort(start);
+
+        for(int i = 0; i < sortedCampuses.count(); i++)
+        {
+            ui->sort_listWidget->addItem(sortedCampuses[i]);
+        }
+    }
 }
 
+
+/*
+ * recursiveCollegeSort()
+ * This function creates the optimal college touring trip plan.
+ * For example, from the starting campus, the next campus will be the closest to the start, then the closest after that.
+ * Each recursive call find the closest campus to the campus passed in.
+ * When the closest is found, the new campus is sorted and then used for the next call.
+ * IN: QString, currentCamp
+ * OUT: nothing returned
+ */
+void planTripWindow::recursiveCollegeSort(QString currentCamp)
+{
+   sortedCampuses.enqueue(currentCamp);
+   selectedCampuses.removeAll(currentCamp);
+
+   if(selectedCampuses.isEmpty())
+   {
+       return;
+   }
+   else
+   {
+        QString* otherCamp = selectedCampuses.begin();
+        int currentIndex = 0;
+
+        double leastDist = m_database.GetDistBtwn(currentCamp, *otherCamp);
+        int leastIndex = 0;
+
+        otherCamp++;
+        currentIndex++;
+
+        while(otherCamp != selectedCampuses.end())
+        {
+            double currentDist = m_database.GetDistBtwn(currentCamp, *otherCamp);
+            if (currentDist < leastDist)
+            {
+                leastDist = currentDist;
+                leastIndex = currentIndex;
+            }
+            else
+            {
+                otherCamp++;
+                currentIndex++;
+            }
+        }
+        QString nextCamp = selectedCampuses.at(leastIndex);
+        recursiveCollegeSort(nextCamp);
+   }
+}
--- plantripwindow.h
+++ plantripwindow.h
@@ -2,6 +2,8 @@
 #define PLANTRIPWINDOW_H
 
 #include <QDialog>
+#include <QMessageBox>
+#include "dbmanager.h"
 
 // This class will give the user options on how they would like to plan their trip
 namespace Ui {
@@ -22,9 +24,15 @@ private slots:
     void on_planCustomTrip_clicked();
 
     void on_startTrip_clicked();
+    bool checkCampusVectorNames(QVector<QString> campuses);
+    void recursiveCollegeSort(QString currentCamp);
 
 private:
     Ui::planTripWindow *ui;
+    dbManager m_database;
+
+    QVector<QString> selectedCampuses; /**< vector of selected campuses for the tour*/
+    QQueue<QString> sortedCampuses;   /**< queue of sorted campuses for tour*/
 };
 
 #endif // PLANTRIPWINDOW_H
