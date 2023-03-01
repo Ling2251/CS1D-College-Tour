a272b72772a133459ba4e3420f494a438ecc31de
Peijun Zhao <pzhao1@saddleback.edu>
2023-02-28 20:50:51 -0800

    Added the functionality of displaying the selected college and their souvenirs

--- plantripwindow.h
+++ plantripwindow.h
@@ -15,8 +15,24 @@ class planTripWindow : public QDialog
     Q_OBJECT
 
 public:
+    struct Souvenir
+    {
+        QString souvName; /**< Souvenir name*/
+        QString campus; /**< Campus of origin*/
+        double cost; /**< Cost of the souvenir */
+        int quantity; /**< Number of souvenirs purchased*/
+    };
+
     explicit planTripWindow(QWidget *parent = nullptr);
     ~planTripWindow();
+
+    bool checkCampusVectorNames(QVector<QString> campuses);
+    void displayCollegeTripList();
+    void recursiveCollegeSort(QString currentCamp);
+    void goToSouvenirShop();
+    void showSelectCampusComboBox(QStringListModel *model);
+    void showSouvTableView(QSqlQueryModel *model);
+    void showSouvCartTableView(QSqlQueryModel *model);
 
 private slots:
     void on_planShortTrip_clicked();
@@ -24,15 +40,18 @@ private slots:
     void on_planCustomTrip_clicked();
 
     void on_startTrip_clicked();
-    bool checkCampusVectorNames(QVector<QString> campuses);
-    void recursiveCollegeSort(QString currentCamp);
+
+    void on_selectCampus_comboBox_currentIndexChanged();
+
+    void on_addSouvenir_button_clicked();
 
 private:
     Ui::planTripWindow *ui;
     dbManager m_database;
-
     QVector<QString> selectedCampuses; /**< vector of selected campuses for the tour*/
     QQueue<QString> sortedCampuses;   /**< queue of sorted campuses for tour*/
+    QStack<Souvenir> souvenirCart;  /**< stack of the purchased souvenirs*/
+    QString sQry;
 };
 
 #endif // PLANTRIPWINDOW_H
